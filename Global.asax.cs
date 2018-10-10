60efa93c75f216a3b4d0914a6cc2d293ad4111c7
Jonathan Read <jread@progress.com>
2018-10-10 14:46:06 -0400

    ugrade to siteifnity 11 latest. added fcmsharp to send push notifications when new news items are added

--- Global.asax.cs
+++ Global.asax.cs
@@ -5,39 +5,91 @@ using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
 using SitefinityWebApp.Mvc.Models;
+using SitefinityWebApp.PushNotifications;
 using SitefinityWebApp.Tasks;
 using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Data;
+using Telerik.Sitefinity.Data.Events;
 using Telerik.Sitefinity.Frontend;
 using Telerik.Sitefinity.Frontend.Navigation.Mvc.Models;
 using Telerik.Sitefinity.GenericContent.Model;
 using Telerik.Sitefinity.Modules.News;
 using Telerik.Sitefinity.News.Model;
 using Telerik.Sitefinity.Security;
+using Telerik.Sitefinity.Services;
 using Telerik.Sitefinity.Workflow;
+using FcmSharp.Requests;
 
 namespace SitefinityWebApp
 {
-    public class Global : System.Web.HttpApplication
-    {
+	public class Global : System.Web.HttpApplication
+	{
 
-        protected void Application_Start(object sender, EventArgs e)
-        {
-            Bootstrapper.Initialized += this.Bootstrapper_Initialized;
-            Bootstrapper.Bootstrapped += Bootstrapper_Bootstrapped;
-        }
+		protected void Application_Start(object sender, EventArgs e)
+		{
+			Bootstrapper.Initialized += this.Bootstrapper_Initialized;
+			Bootstrapper.Bootstrapped += Bootstrapper_Bootstrapped;
+			SystemManager.ApplicationStart += new EventHandler<EventArgs>(ApplicationStartHandler);
+		}
+		private void ApplicationStartHandler(object sender, EventArgs e)
+		{
+			EventHub.Subscribe<IDataEvent>(evt => DataEventHandler(evt));
+		}
 
-        private void Bootstrapper_Initialized(object sender, Telerik.Sitefinity.Data.ExecutedEventArgs e)
-        {
-            if (e.CommandName == "Bootstrapped")
-            {
-                FrontendModule.Current.DependencyResolver.Rebind<INavigationModel>().To<CustomNavigationModel>();
-                //RenewContentTask.CreateTask();
-            }
-        }
+		public void DataEventHandler(IDataEvent eventInfo)
+		{
+			var action = eventInfo.Action;
+			var contentType = eventInfo.ItemType;
+			if (action == "New" && contentType == typeof(NewsItem))
+			{
+				var itemId = eventInfo.ItemId;
+				var providerName = eventInfo.ProviderName;
 
-        protected void Bootstrapper_Bootstrapped(object sender, EventArgs e)
-        {
-            FeatherActionInvokerCustom.Register();
-        }
-    }
+				var manager = ManagerBase.GetMappedManager(contentType, providerName);
+				if (manager.GetItemOrDefault(contentType, itemId) is NewsItem item && item.Status == ContentLifecycleStatus.Master)
+				{
+					// Construct the Data Payload to send:
+					var data = new Dictionary<string, string>()
+									 {
+										{"id", item.Id.ToString()},
+										{"test","testing" }
+									 };
+
+					// The Message should be sent to the News Topic:
+					var message = new FcmMessage()
+					{
+						ValidateOnly = false,
+						Message = new Message
+						{
+							Topic = "news",
+							Data = data,
+							Notification = new Notification()
+							{
+								Body = item.Title,
+								Title = "Hot of the presses!"
+							}
+						}
+					};
+
+					var result = PushNotifications.PushNotifications.SendNotificationFromFirebaseCloud(message);
+					result.ToString();
+				}
+			}
+		}
+
+		private void Bootstrapper_Initialized(object sender, Telerik.Sitefinity.Data.ExecutedEventArgs e)
+		{
+			if (e.CommandName == "Bootstrapped")
+			{
+				FrontendModule.Current.DependencyResolver.Rebind<INavigationModel>().To<CustomNavigationModel>();
+				//RenewContentTask.CreateTask();
+
+			}
+		}
+
+		protected void Bootstrapper_Bootstrapped(object sender, EventArgs e)
+		{
+			FeatherActionInvokerCustom.Register();
+		}
+	}
 }
---
+++ PushNotifications/PushNotifications.cs
@@ -0,0 +1,48 @@
+﻿using FcmSharp;
+using FcmSharp.Requests;
+using FcmSharp.Settings;
+using System;
+using System.Threading;
+using System.Web.Hosting;
+using Telerik.Sitefinity.Abstractions;
+
+namespace SitefinityWebApp.PushNotifications
+{
+	/// <summary>
+	/// Static class for sending push notifications
+	/// </summary>
+	public static class PushNotifications
+	{
+		/// <summary>
+		/// Send push notifications
+		/// </summary>
+		/// <returns>Results from API</returns>
+		public static string SendNotificationFromFirebaseCloud(FcmMessage message)
+		{
+			string result = string.Empty;
+			string serviceKeyPath = HostingEnvironment.MapPath("~/App_Data/Sitefinity/quantum-app-dea2a-firebase-adminsdk-flpe9-5a5652bbb2.json");
+			serviceKeyPath.ToString();
+			var settings = FileBasedFcmClientSettings.CreateFromFile("quantum-app-dea2a", serviceKeyPath);
+
+			// Construct the Client:
+			using (var client = new FcmClient(settings))
+			{
+				// Finally send the Message and wait for the Result:
+				try
+				{
+					CancellationTokenSource cts = new CancellationTokenSource();
+
+					// Send the Message and wait synchronously:
+					var response = client.SendAsync(message, cts.Token).GetAwaiter().GetResult();
+					result = response.Name;
+				}
+				catch (Exception ex)
+				{
+					Log.Write(ex, System.Diagnostics.TraceEventType.Error);
+				}
+
+			}
+			return result;
+		}
+	}
+}
